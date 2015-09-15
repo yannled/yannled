8f17ffec1517edc2ca7357c7a827c3a594afac1e
yannled <Honda CBR 250>
2015-09-15 14:18:35 +0200

    "aurthographe

--- public/js/controllers/AboutCtrl.js
+++ public/js/controllers/AboutCtrl.js
@@ -2,8 +2,30 @@ angular.module('AboutCtrl', []).controller('AboutController', function($scope) {
 	$scope.index = 1;
 	$scope.steps =
     [
-      { firstTitle: 'Apprentissage',secondTitle:'Ecole Polytechnique Fédérale de Lausanne, EPFL', content:"J'ais effectué mon apprentissage au sein du service de l'Enacit2 à l'EPFL, un apprentissage général en informatique m'ayant permis de toucher à une grande partie du métier.", imgSrc:'../../img/EPFL.jpg',linkName:'Enacit2',link:'http://enacit2.epfl.ch/',},
-	  { firstTitle: "Cours d'apprentissage",secondTitle:'Ecole Professionel de Lausanne, EPSIC', content:"Pendant les quatre années d'apprentissage, j'ai suivis, à l'Epsic Mes cours d'informatiques ainsi que mes cours de maturité", imgSrc:'../../img/epsic.png',linkName:'EPSIC',link:'http://www.epsic.ch/', },
-	  { firstTitle: 'Premiers Sites Internet',secondTitle:"", content:"Début des création de site web pour des clients, dans un but lucratif. En partenariat avec Roxanne Borloz, Designeuse.", imgSrc:'../../img/roxanne.png',linkName:'Roxanne Borloz, designeuse',link:'http://www.roxanne-borloz.ch/',},
-	  { firstTitle: 'Obtention De mon CFC et de ma Maturité',secondTitle:"", content:"", imgSrc:'http://placehold.it/200x200', } ];	
+      { firstTitle: 'Apprentissage',
+		  secondTitle:'Ecole Polytechnique Fédérale de Lausanne, EPFL',
+		  content:"Apprentissage effectué au sein du service de l'Enacit2 à l'EPFL, un apprentissage général en informatique m'ayant permis de toucher à une grande partie du métier.", imgSrc:'../../img/EPFL.jpg',
+		  linkName:'Enacit2',
+		  link:'http://enacit2.epfl.ch/',},
+		  
+		  
+	  { firstTitle: "Cours d'apprentissage",
+		  secondTitle:'Ecole Professionel de Lausanne, EPSIC',
+		  content:"Cours de maturité professionel et d'informatique suivis à l'Epsic.",
+		  imgSrc:'../../img/epsic.png',
+		  linkName:'EPSIC',
+		  link:'http://www.epsic.ch/', },
+		  
+		  
+	  { firstTitle: 'Premiers Sites Internet',
+		  secondTitle:"",
+		  content:"Premier site web pour un client, en partenariat avec Roxanne Borloz, Graphiste.",
+		  imgSrc:'../../img/roxanne.png',
+		  linkName:'Roxanne Borloz, Graphiste ',
+		  link:'http://www.roxanne-borloz.ch/',},
+		  
+	  { firstTitle: 'Obtention De mon CFC et de ma Maturité',
+		  secondTitle:"",
+		  content:"",
+		  imgSrc:'http://placehold.it/200x200', } ];	
 });
