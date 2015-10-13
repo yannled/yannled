7b8fafa7175d541a8610da5aa9ddc8b3bdb50e60
yannled <Honda CBR 250>
2015-10-13 19:35:27 +0200

    13:10/19:35

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,8 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 	$scope.title = "Yann Lederrey";
-	$scope.tagline = 'Votre site selon vos besoins !';	
+	$scope.tagline = 'Votre site selon vos besoins !';
+	$scope.tagline2 = 'Des prix bas sur le marché !';
+	$scope.tagline3 = 'Adapté aux smartphone et tablettes !';	
 	$scope.templates =
     [ { name: 'services.html', url: 'PartialViews/services.html'},
       { name: 'portfolio.html', url: 'PartialViews/portfolio.html'},
