9e91bb6f663fccdfc493aec1c0931a37be033576
yannled <Honda CBR 250>
2016-01-28 13:58:53 +0100

    ajout portfolio

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,7 +1,7 @@
 angular.module('PortfolioCtrl', []).controller('PortfolioController', function($scope) {
 	$scope.steps =
-    [ { imgSrc:'../../img/noway.png' ,link:'http://www.no-way-band.com/', },
+    [ { imgSrc:'../../img/noway.png' ,link:'http://yannlederrey.wix.com/no-way', },
       { imgSrc:'../../img/sitevente.png',link:'http://yannlederreytest.ch/',},
-	  // { imgSrc:'http://placehold.it/300x300',link:'#', },
+	  { imgSrc:'../../img/Logo_Championnats_Suisses.jpg',link:'http://echallens2016.ch/', },
 	];
 });
