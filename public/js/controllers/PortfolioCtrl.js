175c1051321471ee684fc5021424cda1a0eeac49
yannled <yannlederrey@gmail.com>
2016-06-10 10:39:19 +0200

    modif portfolio

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,11 +1,11 @@
 angular.module('PortfolioCtrl', []).controller('PortfolioController', function($scope) {
 	$scope.steps =
-    [ { imgSrc:'../../img/noway.png' ,link:'http://yannlederrey.wix.com/no-way', },
-      { imgSrc:'../../img/sitevente.png',link:'http://yannlederreytest.ch/',},
+    [ { imgSrc:'../../img/noway.png' ,link:'http://yannlederrey.wix.com/no-way', },      
 	  { imgSrc:'../../img/Logo_Championnats_Suisses.jpg',link:'http://echallens2016.ch/', },
 	];
 	$scope.steps2 =
     [
 	 { imgSrc:'../../img/Chabloz.PNG',link:'http://roxannetest.tk/chabloz', },
+	 { imgSrc:'../../img/sitevente.png',link:'http://yannlederreytest.ch/',},
 	];
 });
