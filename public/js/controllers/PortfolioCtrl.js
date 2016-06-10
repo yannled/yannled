414a59f27ad75034282d61813587dbd076bf4f2e
yannled <yannlederrey@gmail.com>
2016-06-10 12:01:36 +0200

    modif portfolio aimant

--- public/js/controllers/PortfolioCtrl.js
+++ public/js/controllers/PortfolioCtrl.js
@@ -1,11 +1,11 @@
 angular.module('PortfolioCtrl', []).controller('PortfolioController', function($scope) {
 	$scope.steps =
     [ { imgSrc:'../../img/noway-aimant.png' ,link:'http://yannlederrey.wix.com/no-way', },      
-	  { imgSrc:'../../img/Logo_Championnats_Suisses.jpg',link:'http://echallens2016.ch/', },
+	  { imgSrc:'../../img/Logo_Championnats_Suisses-aimant.png',link:'http://echallens2016.ch/', },
 	];
 	$scope.steps2 =
     [
-	 { imgSrc:'../../img/Chabloz.PNG',link:'http://roxannetest.tk/chabloz', },
-	 { imgSrc:'../../img/sitevente.png',link:'http://yannlederreytest.ch/',},
+	 { imgSrc:'../../img/Chabloz-aimant.png',link:'http://roxannetest.tk/chabloz', },
+	 { imgSrc:'../../img/sitevente-aimant.png',link:'http://yannlederreytest.ch/',},
 	];
 });
